dfc4cb33b8ffdd647e1424f356304961c388933b
黄院 <2171077189@qq.com>
2023-10-24 15:55:18 +0800

    deploy

--- build.sh
+++ build.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:jsweeta/jsweeta.github.io.git master
+# git push -f git@github.com:jsweeta/jsweeta.github.io.git master
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# 如果发布到 https://<USERNAME>.github.io/home
+git push -f git@github.com:jsweeta/home.git master:gh-pages
 
 cd -
