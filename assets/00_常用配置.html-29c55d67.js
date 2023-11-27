bbb2f1ec45078411554623f24e5cc2296e7702f0
Jsweet <2171077189@qq.com>
2023-11-27 20:21:11 +0800

    Updates

---
+++ assets/00_常用配置.html-29c55d67.js
@@ -0,0 +1,8 @@
+import{_ as n}from"./plugin-vue_export-helper-c27b6911.js";import{r,o as i,c as s,b as e,d as t,e as d,a as o}from"./app-85da6326.js";const c={},l=e("h1",{id:"frontmatter-配置",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#frontmatter-配置","aria-hidden":"true"},"#"),t(" frontmatter 配置")],-1),h={href:"https://theme-hope.vuejs.press/zh/config/frontmatter/info.html#author",target:"_blank",rel:"noopener noreferrer"},m=o(`<h3 id="常用项" tabindex="-1"><a class="header-anchor" href="#常用项" aria-hidden="true">#</a> 常用项</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>title: 标题
+descritption: 描述
+category: 分类
+tag: 标签
+star: 是否收藏
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="可选项" tabindex="-1"><a class="header-anchor" href="#可选项" aria-hidden="true">#</a> 可选项</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>image: 预览图
+order: 侧边栏顺序
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div>`,4);function u(_,v){const a=r("ExternalLinkIcon");return i(),s("div",null,[l,e("blockquote",null,[e("p",null,[e("a",h,[t("https://theme-hope.vuejs.press/zh/config/frontmatter/info.html#author"),d(a)])])]),m])}const b=n(c,[["render",u],["__file","00_常用配置.html.vue"]]);export{b as default};
